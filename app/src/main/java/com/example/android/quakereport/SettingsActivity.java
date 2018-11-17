18e9acf7780813a38c5286d290f019e61e991b4c
NaotoSama <42671784+NaotoSama@users.noreply.github.com>
2018-11-17 18:28:58 +0800

    Preference Summaries (added more annotations)

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -24,7 +24,8 @@ public class SettingsActivity extends AppCompatActivity {
      Then when a single Preference has been changed by the user and is about to be saved, the onPreferenceChange() method will be invoked with the key of the preference that was changed.
      Note that this method returns a boolean, which allows us to prevent a proposed preference change by returning false.
      First declare that the EarthquakePreferenceFragment class should implement the OnPreferenceChangeListener interface, and override the onPreferenceChange() method.
-     The code in onPreferenceChange() method takes care of updating the displayed preference summary after it has been changed. */
+     The code in onPreferenceChange() method takes care of updating the displayed preference summary after it has been changed.
+     因為 OnPreferenceChangeListener 是一個接口，所以需要在 EarthquakePreferenceFragment 類名後添加 implements 表示在這個類內實現接口*/
     public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
         //Override the onCreate() method to use the settings_main XML resource that we defined earlier.
@@ -37,25 +38,29 @@ public class SettingsActivity extends AppCompatActivity {
             //We need to update the preference summary when the settings activity is launched.
             //Given the key of a preference, we can use PreferenceFragment's findPreference() method to get the Preference object,
             //and setup the preference using a helper method called bindPreferenceSummaryToValue().
-            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
-            bindPreferenceSummaryToValue(minMagnitude);
+            //在啟動設置活動後，仍需要更新偏好摘要。如果給定偏好項的key，就可以用 PreferenceFragment 的 findPreference() 方法來獲取偏好對象，然後使用 bindPreferenceSummaryToValue()來設置偏好。
+            //在 onCreate 內透過 findPreference 找到偏好項並導入輔助方法(bindPreferenceSummaryToValue)
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key)); //透過 findPreference取得偏好項的識別key，找到minMagnitude這個偏好項的欄位
+            bindPreferenceSummaryToValue(minMagnitude); // 叫bindPreferenceSummaryToValue輔助方法去套用到minMagnitude這個偏好項的欄位上
         }
 
         @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
-            preference.setSummary(stringValue);
+        //在這裡將下面bindPreferenceSummaryToValue輔助方法傳入的偏好項的預設數值透過setSummary 顯示在偏好項標題的下方。
+        public boolean onPreferenceChange(Preference preference, Object value) {  //宣告onPreferenceChange方法並導入兩個arguments:偏好項以及數值物件
+            String stringValue = value.toString(); //onPreferenceChange已透過下面的bindPreferenceSummaryToValue輔助方法載入了監聽器監聽到的(用戶輸入的)預設數值，所以只須把數值(value)轉換成字符，並命名為stringValue
+            preference.setSummary(stringValue);  //透過setSummary把預設數值顯示在偏好項標題的下方。
             return true;
         }
 
         //we need to define the bindPreferenceSummaryToValue() helper method to set the current EarthquakePreferenceFragment instance as the listener on each preference.
         //We also read the current value of the preference stored in the SharedPreferences on the device,
         //and display that in the preference summary (so that the user can see the current value of the preference).
+        //在此輔助方法內，設置導入的偏好項的監聽器，創建偏好項的SharedPreferences對象並通過 getString 獲取偏好項的值，最後傳給上面的onPreferenceChange方法來處理。
         private void bindPreferenceSummaryToValue(Preference preference) {  //這是輔助方法
-            preference.setOnPreferenceChangeListener(this);
-            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
-            String preferenceString = preferences.getString(preference.getKey(), "");
-            onPreferenceChange(preference, preferenceString);
+            preference.setOnPreferenceChangeListener(this); //對偏好項綁定監聽器
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext()); //叫監聽器去監聽偏好項的內容
+            String preferenceString = preferences.getString(preference.getKey(), ""); //去取得偏好項的識別key，獲取該識別key下的預設數值，將該值命名為preferenceString
+            onPreferenceChange(preference, preferenceString); //把預設數值傳給上面的onPreferenceChange方法
         }
     }
 
